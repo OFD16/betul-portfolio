4332c535335f6d826f59180cf8efa8adb9b8aeba
Ömer Faruk Demirsoy <omerdmrsy@gmail.com>
2023-06-11 11:16:40 +0300

    feat: new components created
    - expander_card for my_projects and my_blogs screens
    - hover_button for expander_card

---
+++ lib/components/expander_card.dart
@@ -0,0 +1,148 @@
+import 'dart:async';
+
+import 'package:betul_portfolio/components/hover_button.dart';
+import 'package:betul_portfolio/design/app_colors.dart';
+import 'package:betul_portfolio/design/app_text_styles.dart';
+import 'package:delayed_display/delayed_display.dart';
+import 'package:flutter/material.dart';
+
+class ExpanderCard extends StatefulWidget {
+  final String title;
+  final String description;
+  final String? imageUrl;
+  const ExpanderCard(
+      {Key? key, required this.title, required this.description, this.imageUrl})
+      : super(key: key);
+
+  @override
+  State<ExpanderCard> createState() => _ExpanderCardState();
+}
+
+class _ExpanderCardState extends State<ExpanderCard> {
+  bool _isHovered = false;
+  double containerHeight = 252;
+  Timer? _exitTimer;
+
+  @override
+  void dispose() {
+    _exitTimer?.cancel();
+    super.dispose();
+  }
+
+  void onEnter(_) => setState(() {
+        _isHovered = true;
+        containerHeight = 571;
+      });
+
+  void onExit(_) {
+    //TO DO: sonsuz defa gir çık yapıp üstünde bekleyince buglanıyor
+    // ama sonra dışarda bekleyip tekrar girince çözülüyor
+    _exitTimer?.cancel(); // Cancel any pending delayed execution
+    setState(() {
+      _isHovered = false;
+    });
+
+    _exitTimer = Timer(const Duration(seconds: 1), () {
+      setState(() {
+        containerHeight = 252;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: onEnter,
+      onExit: onExit,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          AnimatedContainer(
+            width: 462,
+            height: containerHeight,
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 2),
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              shape: BoxShape.rectangle,
+              color: AppColors.exPink,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.7),
+                  spreadRadius: 2,
+                  blurRadius: 0,
+                  offset: const Offset(4, 4),
+                ),
+              ],
+            ),
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 200,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: AnimatedOpacity(
+                    opacity: _isHovered ? 1.0 : 0.0,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.easeInOut,
+                    child: DelayedDisplay(
+                      delay: const Duration(milliseconds: 500),
+                      fadingDuration: const Duration(milliseconds: 500),
+                      slidingBeginOffset: const Offset(0.0, -0.2),
+                      slidingCurve: Curves.easeInOut,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(widget.title, style: AppTextStyles.cardTitle),
+                          const SizedBox(height: 24),
+                          Text(
+                            widget.description,
+                            style: AppTextStyles.expandedCardDescription,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 4,
+                          ),
+                          const SizedBox(height: 55),
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(),
+                              HoverButton(),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: -45,
+            left: 45,
+            child: Container(
+              width: 444,
+              height: 252,
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                shape: BoxShape.rectangle,
+                image: DecorationImage(
+                  image: NetworkImage(widget.imageUrl ??
+                      'https://images.ctfassets.net/hrltx12pl8hq/7JnR6tVVwDyUM8Cbci3GtJ/bf74366cff2ba271471725d0b0ef418c/shutterstock_376532611-og.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
