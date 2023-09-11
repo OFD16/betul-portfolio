4374daf4ac9fd9830481d20f27ee0641ce3f45b6
Ömer Faruk Demirsoy <96663769+OFD16@users.noreply.github.com>
2023-09-11 16:09:52 +0300

    feat: projects screen UI finished

--- lib/screens/myProjectsScreen/components/midSection.dart
+++ lib/screens/myProjectsScreen/components/midSection.dart
@@ -1,57 +1,69 @@
-import 'package:betul_portfolio/components/blog_card.dart';
-import 'package:betul_portfolio/components/circle_arrow_icon_button.dart';
-import 'package:betul_portfolio/components/expander_card.dart';
-import 'package:betul_portfolio/components/lister_content.dart';
-
-import 'package:betul_portfolio/types/arrow_direction.dart';
 import 'package:flutter/material.dart';
+import 'package:betul_portfolio/components/expander_card.dart';
 
-final data = [
-  {
-    'title': 'Başlık',
-    'id': 'a1',
-    'image':
-        'https://www.shutterstock.com/image-photo/word-example-written-on-magnifying-260nw-1883859943.jpg',
-    'description': 'I am Omar.'
-  },
-  {
-    'id': 'a2',
-    'image':
-        'https://t3.ftcdn.net/jpg/00/92/53/56/360_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg',
-    'description':
-        'This is another item. asdghfjasdghfasdghasdhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfhfjasdghfasdghasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfaasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfsdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghfasdghffjdhgsaafasagsaafasasdghfjdhgsaafasasdghfjdhgsaafasasdghfjdhgsaafasasdghfjdhgsaafasdhgsaafas',
-  },
-  {
-    'id': 'a3',
-    'image':
-        'https://thumbs.dreamstime.com/b/example-red-tag-example-red-square-price-tag-117502755.jpg',
-    'description': 'And here is one more item.',
-  }
-];
 onPressIcon() => {print('İcona basıldı')};
-var item = data[1];
+
+var projectsList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+var leftContentList =
+    projectsList.where((element) => element % 2 == 0).toList();
+var rightContentList =
+    projectsList.where((element) => element % 2 != 0).toList();
 
 Widget midSection() {
-  return const Column(
-    children: [
-      ExpanderCard(
-        title: 'Redesign',
-        description:
-            'Lorem ipsum dolor sit amet consectetur. Est adipiscing neque adipiscing nunc dignissim cursus vitae. Vel consequat amet ',
-      )
-    ],
+  // print('leftContentList: $leftContentList');
+  // print('rightContentList: $rightContentList');
+
+  return Container(
+    alignment: Alignment.center,
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: leftContentList.map((e) {
+              return Container(
+                padding: const EdgeInsets.symmetric(vertical: 55),
+                child: const ExpanderCard(
+                  title: 'Redesign',
+                  description:
+                      'Lorem ipsum dolor sit amet consectetur. Est adipiscing neque adipiscing nunc dignissim cursus vitae. Vel consequat amet ',
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+        const SizedBox(width: 55),
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: rightContentList.map((e) {
+              return Container(
+                padding: const EdgeInsets.symmetric(vertical: 55),
+                child: const ExpanderCard(
+                  title: 'Redesign',
+                  description:
+                      'Lorem ipsum dolor sit amet consectetur. Est adipiscing neque adipiscing nunc dignissim cursus vitae. Vel consequat amet ',
+                ),
+              );
+            }).toList(),
+          ),
+        )
+      ],
+    ),
   );
 }
 
-Widget content(void Function()? onPressIcon) {
-  return Row(
-    children: [
-      BlogCard(item: item),
-      const SizedBox(width: 30),
-      CircleIconButton(
-        onPressIcon: onPressIcon,
-        arrowDirection: ArrowDirection.right,
-      ),
-    ],
-  );
-}
+// Widget content(void Function()? onPressIcon) {
+//   return Row(
+//     children: [
+//       BlogCard(item: item),
+//       const SizedBox(width: 30),
+//       CircleIconButton(
+//         onPressIcon: onPressIcon,
+//         arrowDirection: ArrowDirection.right,
+//       ),
+//     ],
+//   );
+// }
