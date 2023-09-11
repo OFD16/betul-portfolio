a0ebd7058c9c31a54976e2f8ec9e2db5fda85276
Ömer Faruk Demirsoy <96663769+OFD16@users.noreply.github.com>
2023-09-11 16:09:05 +0300

    feat: started about screen

--- lib/screens/aboutScreen/about_view.dart
+++ lib/screens/aboutScreen/about_view.dart
@@ -1,4 +1,6 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:betul_portfolio/components/top_section.dart';
+import 'package:betul_portfolio/screens/aboutScreen/components/bodyWidget.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -12,6 +14,21 @@ class AboutScreen extends StatefulWidget {
 class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            FittedBox(
+                child: TopSection(
+              body: bodyWidget(context),
+            )),
+            const SizedBox(height: 90),
+            // midSection(),
+            // const SizedBox(height: 240),
+            // const Footer(),
+          ],
+        ),
+      ),
+    );
   }
 }
